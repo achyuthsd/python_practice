3001522c7b3f3af17399e89c14436f20f99fc785
Achyuth S <165375463+achyuthsd@users.noreply.github.com>
2026-07-22 14:13:46 +0530

    python

---
+++ main.py
@@ -0,0 +1,257 @@
+# Check whether a number is positive, negative, or zero.
+
+# n = int(input('Enter the number'))
+# if n > 0:
+#     print('Positive')
+# elif n < 0:
+#     print('Negative')
+# else:
+#     print('Zero')
+
+
+
+# Check whether a number is even or odd.
+
+# n = int(input('Enter the number'))
+# if n % 2 == 0:
+#     print('Even')
+# elif n % 2 != 0:
+#     print('Odd')
+# else:
+#     print('Invalid')
+
+
+
+# Find the greater of two numbers.
+
+# num1 = int(input('Enter first number'))
+# num2 = int(input('Enter second number'))
+# if num1 > num2:
+#     print(f'{num1} is greater')
+# elif num1 < num2:
+#     print(f'{num2} is greater')
+# else:
+#     print('Both numbers are the same')
+
+
+
+# Find the greatest of three numbers.
+
+# num1 = int(input('Enter first number'))
+# num2 = int(input('Enter second number'))
+# num3 = int(input('Enter third number'))
+#
+# if num1 > num2:
+#     if num1 > num3:
+#         print(f'{num1} is greatest')
+#     else:
+#         print(f'{num3} is greatest')
+# else:
+#     if num2 > num3:
+#         print(f'{num2} is greatest')
+#     else:
+#         print(f'{num3} is greatest')
+
+
+
+# Find the smallest of three numbers.
+
+# num1 = int(input('Enter first number'))
+# num2 = int(input('Enter second number'))
+# num3 = int(input('Enter third number'))
+#
+# if num1 < num2:
+#     if num1 < num3:
+#         print(f'{num1} is smallest')
+#     else:
+#         print(f'{num3} is smallest')
+# else:
+#     if num2 < num3:
+#         print(f'{num2} is smallest')
+#     else:
+#         print(f'{num3} is smallest')
+
+
+
+# Check whether a person is eligible to vote.
+
+# age = int(input('Enter your age'))
+# if age >= 18:
+#     print('Eligible')
+# else:
+#     print('Not Eligible')
+
+
+
+# Check whether a student passed or failed (pass mark = 40).
+
+# mark = int(input('Enter the marks'))
+# if mark >= 40:
+#     print('Pass')
+# else:
+#     print('Fail')
+
+
+
+# Display grade based on marks (A, B, C, D, Fail).
+
+# mark = int(input('Enter the marks'))
+# if mark >= 90:
+#     print('A')
+# elif mark >= 80:
+#     print('B')
+# elif mark >= 70:
+#     print('C')
+# elif mark >= 60:
+#     print('D')
+# else:
+#     print('Fail')
+
+
+
+# Check whether a year is a leap year.
+
+# year = int(input('Enter the year'))
+# if year % 100 == 0:
+#     if year % 400 == 0:
+#         print('Leap year')
+#     else:
+#         print('Non leap year')
+# else:
+#     if year % 4 == 0:
+#         print('Leap year')
+#     else:
+#         print('Non Leap year')
+
+
+
+# Check whether a number is divisible by 5.
+
+# n = int(input('Enter the number'))
+# if n % 5 == 0:
+#     print('Number is divisible by 5')
+# else:
+#     print('Number not divisible by 5')
+
+
+
+# Check whether a number is divisible by both 3 and 5.
+
+# n = int(input('Enter the number'))
+# if n % 3 == 0 and n % 5 == 0:
+#     print('Number is divisible')
+# else:
+#     print('Number is not divisible')
+
+
+
+# Check whether a character is a vowel or consonant.
+
+# vowels = 'aeiou'
+# character = input('Enter the character')
+# if character in vowels:
+#     print('Vowel')
+# else:
+#     print('Consonent')
+
+
+
+# Check whether a character is uppercase or lowercase.
+
+# character = input('Enter the character')
+# if character == character.upper():
+#     print('Character is upper case')
+# else:
+#     print('Character is lower case')
+
+
+
+
+# Check whether a number is a single-digit, two-digit, or three-digit number.
+
+# n = input('Enter the number')
+# if len(n) == 1:
+#     print('Single digit')
+# elif len(n) == 2:
+#     print('Double digit')
+# elif len(n) == 3:
+#     print('Triple digit')
+# else:
+#     print('More than 3 digit')
+
+
+
+# Check whether a person is eligible for a driving license (age ≥ 18).
+
+# age = int(input('Enter your age'))
+# if age >= 18:
+#     print('Eligible')
+# else:
+#     print('Not Eligible')
+
+
+
+# Check whether a number is between 1 and 100.
+
+# n = int(input('Enter the number'))
+# if n >= 1 and n <= 100:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+# Check whether a salary is eligible for a bonus (e.g., salary ≥ 30000).
+
+# salary = int(input('Enter the salary'))
+# if salary >= 30000:
+#     print('Eligible')
+# else:
+#     print('Not Eligible')
+
+
+
+# Calculate a discount based on the purchase amount.
+
+# amt = int(input('Enter the amount'))
+# dis = int(input('Enter the discount %'))
+# d = 100 - dis
+# print(amt*(d/100))
+
+
+
+# Check whether a triangle is valid using three angles.
+
+# a1 = int(input('Enter the first angle'))
+# a2 = int(input('Enter the second angle'))
+# a3 = int(input('Enter the third angle'))
+# if a1 > 0 and a2 > 0 and a3 > 0 and ((a1 + a2+ a3) == 180):
+#     print('Yes its triangle')
+# else:
+#     print('No its not triangle')
+
+
+# Display the day of the week based on a number (1–7 using elif).
+n = int(input('Enter the number'))
+match n:
+    case 1:print('Sunday')
+    case 2:print('Monday')
+    case 3:print('Tuesday')
+    case 4:print('Wednesday')
+    case 5:print('Thursday')
+    case 6:print('Friday')
+    case 7:print('Saturday')
+    case _:print('Enter a valid date')
+
+
+
+
+
+
+
+
+
+
+
+
+
